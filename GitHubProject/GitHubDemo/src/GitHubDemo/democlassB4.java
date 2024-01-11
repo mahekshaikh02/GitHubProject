c58307fed226b48cf52c11245c633b21a1ddb3fb
Mahek Shaikh <mahekshk02@gmail.com>
2024-01-11 09:04:07 +0530

    commit

---
+++ GitHubProject/GitHubDemo/src/GitHubDemo/democlassB4.java
@@ -0,0 +1,14 @@
+package GitHubDemo;
+
+public class democlassB4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("GitHub Hello World");
+
+	}
+    
+	void display() {
+		System.out.println("branch ");
+	}
+}
