82b13be7a1cb1c367b0954f9755018755c389c74
Mahek Shaikh <mahekshk02@gmail.com>
2024-01-18 09:47:22 +0530

    Commit statement

---
+++ GitHubProject/GitHubDemo/src/GitHubDemo/AddNumbs.java
@@ -0,0 +1,17 @@
+package GitHubDemo;
+
+public class AddNumbs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    
+		int x =1;
+		int y = 2;
+		int sum = x+y;
+		 
+		System.out.println("The sum of the 2 numbers is: "+sum);
+		
+		
+	
+	}
+}
