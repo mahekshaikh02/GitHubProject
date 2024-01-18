2f1b20b987279555e4e9b861b22d4ef748ea1211
Mahek Shaikh <mahekshk02@gmail.com>
2024-01-18 10:27:38 +0530

    commit

--- GitHubProject/GitHubDemo/src/GitHubDemo/AddNumbs.java
+++ GitHubProject/GitHubDemo/src/GitHubDemo/AddNumbs.java
@@ -1,17 +1,32 @@
 package GitHubDemo;
 
 public class AddNumbs {
+	
+	static void subtract() {
+		int x = 12;
+		int y = 6;
+		int sub = x -y;
+	    System.out.println("The difference of the 2 numbers is "+ sub);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    
+   
 		int x =1;
 		int y = 2;
-		int sum = x+y;
-		 
+		int sum = x+y;		 
 		System.out.println("The sum of the 2 numbers is: "+sum);
 		
+		subtract();
 		
-	
 	}
+	
+	
+
+    
 }
+
+  
+
+
+ 
